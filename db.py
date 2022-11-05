e99cb623e6f32d1667e1dd050c39629c0adf6f87
aruntemme <arungauthamk@gmail.com>
2022-11-05 14:32:47 +0530

    removed old files

--- db.py
+++
@@ -1,6 +0,0 @@
-from pymongo import MongoClient
-import urllib 
-
-client = MongoClient("mongodb+srv://admin:"+ urllib.parse.quote("admin@123")+"@database.roxntvt.mongodb.net/?retryWrites=true&w=majority")
-db = client.gifs
-
